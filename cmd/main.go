e66d98fbd828dc035e64a0210b3cd84076c834ed
Matheus <matheusrenan.95@gmail.com>
2024-06-15 17:34:48 -0300

    initial commit

---
+++ cmd/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"log"
+	"net"
+)
+
+func main() {
+	listener, err := net.Listen("tcp", "localhost:8000")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Print(err)
+			continue
+		}
+
+		go handleConn(conn)
+	}
+}
+
+func handleConn(c net.Conn) {
+	defer c.Close()
+
+	for {
+
+	}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/matheusrb95/endrok
+
+go 1.22.4
